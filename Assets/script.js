44ef75015fb6014624a6dd66650359569565a5a5
unknown <jozelinesuarez@gmail.com>
2023-10-11 12:47:12 -0500

    Changed it from being the same day, different hour to be a different day for each weather card

--- Assets/script.js
+++ Assets/script.js
@@ -4,57 +4,63 @@ const searchButton = document.querySelector('.search-button');
 const contentDiv = document.querySelector('.content');
 
 const cityInput = document.querySelector('#cityInput');
+const city = cityInput.value;
+const apiKey = '3557814581c42da838a31e2f756f4234'; // API key for OpenWeatherMap API
+const apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
 
-
-
-searchButton.addEventListener('click', (e) => {
+searchButton.addEventListener("click", (e) => {
   e.preventDefault();
-  const city = cityInput.value;
-  const apiKey = '3557814581c42da838a31e2f756f4234';
-  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+  
   fetch(apiUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((data) => {
-    mainCard.innerHTML = '';
-    weatherCards.innerHTML = '';
+    .then(response => 
+      response.json())
+    .then(data => {
 
-    const forecastList = data.list;
+      const forecastDays = [];
+      const fiveDaysForecast = data.list.filter(forecast => {
+          const forecastDate = new Date(forecast.dt_txt).getDate();
+          if (!forecastDays.includes(forecastDate)) {
+                return forecastDays.push(forecastDate);
+            }
+        });
+ 
+        cityInput.value = "";
+        mainCard.innerHTML = "";
+        weatherCards.innerHTML = "";
 
-    for (let i = 0; i < 6; i++) {
-      const forecast = forecastList[i];
-      const date = forecast.dt_txt;
+        fiveDaysForecast.forEach((forecast, index) => {
+          
+      const date = forecast.dt_txt.split(" ")[0];
       const temperature = forecast.main.temp;
       const windSpeed = forecast.wind.speed;
       const humidity = forecast.main.humidity;
-      console.log(data);
-
-      if (i === 0) {
+      
+      if (index === 0) {
         const cardHeader = document.createElement('section');
         cardHeader.innerHTML = '<h1>' + city + ' ' + '(' + date + ')' + '</h1>' +
-                                  '<h4>Temp: ' + temperature + 'F</h4>'
-                                  + '<h4>Wind: ' + windSpeed + 'MPH</h4>'
-                                  + '<h4>Humidity: ' + humidity + '%</h4>';
+                                  '<h4>Temp: ' + temperature + ' F</h4>'
+                                  + '<h4>Wind: ' + windSpeed + ' MPH</h4>'
+                                  + '<h4>Humidity: ' + humidity + ' %</h4>';
         mainCard.appendChild(cardHeader);
         
       } else {
         const card = document.createElement('section');
         card.innerHTML = '<h4>' + date + '</h4>' +
-                            '<h4>Temp: ' + temperature + 'F</h4>'
-                            + '<h4>Wind: ' + windSpeed + 'MPH</h4>'
-                            + '<h4>Humidity: ' + humidity + '%</h4>';
+                            '<h4>Temp: ' + temperature + ' F</h4>'
+                            + '<h4>Wind: ' + windSpeed + ' MPH</h4>'
+                            + '<h4>Humidity: ' + humidity + ' %</h4>';
         weatherCards.appendChild(card);                      
-      }                 
-    } 
-    
+      }        
+      }); 
+        
     })
     .catch((error) => {
-    console.error('There was a problem with the fetch operation:', error);
-    })
-})
-  
+        console.log('Unable to fetch data from api', error);
+    });
+}
+)
+
+
+
+
